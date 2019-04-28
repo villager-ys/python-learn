2c200b15fde35670d78f09a83ad01a37be51de81
yuanshuai <1179325921@qq.com>
2019-04-28 10:31:28 +0800

    日期模块

---
+++ time_test.py
@@ -0,0 +1,17 @@
+# 时间日期test
+import time
+import datetime
+
+print(time.time())  # 当前时间戳
+time.sleep(2)  # 休眠2秒
+print(time.localtime())  # 格式化时间戳为本地时间
+print(time.strftime('%Y-%m-%d %H:%M:%S'))  # 指定格式格式化时间 返回字符串
+
+# 计算日期相加相减
+print(datetime.datetime.now())
+new_datetime = datetime.timedelta(minutes=10) # 日期偏移量
+print(datetime.datetime.now() + new_datetime)
+
+one_day = datetime.datetime(2019, 4, 28)
+new_datetime = datetime.timedelta(days=10)
+print(one_day + new_datetime)
