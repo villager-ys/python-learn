9074e12c34833775a70b029ce336be4c98a0fa89
yuanshuai <1179325921@qq.com>
2019-04-30 11:09:08 +0800

    多层索引

--- pandas_test.py
+++ pandas_test.py
@@ -18,3 +18,23 @@ print(obj3)
 # 更换obj3的index
 obj3.index = {'hz', 'nj', 'bj'}
 print(obj3)
+
+#dataframe 类似于电子表格的形式
+data = {'city': ['上海','杭州','北京'],
+        'year':[2017,2018,2019],
+        'salary':[3000.4,4008.8,50047.9]}
+frame = DataFrame(data)
+frame2 = DataFrame(data,columns=['city','salary','year'])
+print(frame)
+print(frame2)
+print(frame2.year)
+print(frame2['city'])
+
+# 层次化索引
+import numpy as np
+data2 = Series(np.random.randn(10),index=[['a','a','a','b','b','b','c','c','d','d'],
+                                  [1,2,3,1,2,3,1,2,2,4]])
+print(data2)
+# 转成类似dataframe二维数据
+print(data2.unstack())
+
